ae8436c4c77f9a982929b19abb30d31681d44d77
Jery Hanks <jerryhanksokafor@gmail.com>
2018-06-20 19:15:40 +0100

    fix : use params and not param

--- functions/src/actions/index.ts
+++ functions/src/actions/index.ts
@@ -6,8 +6,8 @@ import { firestoreInstance } from "../index";
 
 export async function updateReadersCount(event) {
 
-    const lessonId = event.param.lessonId
-    const sessionId = event.param.sessionId
+    const lessonId = event.params.lessonId
+    const sessionId = event.params.sessionId
 
     const lessonRef = firestoreInstance.collection(BOOKS_COLLECTION)
         .doc(sessionId).collection(LESSONS_COLLECTION).doc(lessonId);
@@ -19,7 +19,7 @@ export async function updateReadersCount(event) {
         var oldCount = doc.data().readers
         var newCount = oldCount + 1
 
-        const updateDoc = await lessonRef.set({
+        await lessonRef.set({
             readers: newCount
         }, { merge: true })
     } else {
